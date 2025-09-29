312d0fcba3be3bbd5c325fff5622c1f8fc407139
Diego Garcia <tu_correo_de_github@example.com>
2025-09-29 09:40:43 -0300

    Se creó la relación 1:1 de User a Profile (de la Base de Datos: SEQUELIZE)

---
+++ src/models/sequelize/profile.model.js
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+import {UserModel} from "../../models/sequelize/user.model.js"
+
+export const ProfileModel = sequelize.define("Profile", {
+  employee_number: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    unique: true,
+  },
+  first_name: { type: DataTypes.STRING(50), allowNull: false },
+  last_name: { type: DataTypes.STRING(50), allowNull: false },
+  phone: { type: DataTypes.STRING(20), allowNull: true },
+});
+
+// TODO: Relación uno a uno con User (1 User tiene 1 Profile)
+// * 1:1 Profile ↔ User
+// * 'profile' (User) y 'user' (Profile)
+// ! FALTA COMPLETAR ACA
+UserModel.belongsTo(ProfileModel, {foreignKey: "profile_id", as: "profile"});
+
+ProfileModel.hasOne(UserModel, {foreignKey: "profile_id"})
